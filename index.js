360aaae6e8eecbac637edf5e1862890208192cf9
erichua <erichua@webank.com>
2019-10-30 15:51:22 +0800

    init

---
+++ index.js
@@ -0,0 +1,48 @@
+const consoleMethod = [
+  'debug',
+  'error',
+  'info',
+  'log',
+  'warn',
+  'dir',
+  'dirxml',
+  'table',
+  'trace',
+  'group',
+  'groupCollapsed',
+  'groupEnd',
+  'clear',
+  'count',
+  'countReset',
+  'assert',
+  'profile',
+  'profileEnd',
+  'time',
+  'timeLog',
+  'timeEnd',
+  'timeStamp'
+]
+
+const polyfillConsole = () => {
+  consoleMethod.forEach(key => {
+    if (!console['groupCollapsed'] && console['group']) {
+      console['groupCollapsed'] = console['group']
+    }
+    if (!console[key]) {
+      console[key] = () => {
+        console.warn(`console.${key} not supported in this RN version.`)
+      }
+    }
+  })
+}
+
+const disableConsole = () => {
+  consoleMethod.forEach(key => {
+    console[key] = () => { }
+  })
+}
+
+export {
+  polyfillConsole,
+  disableConsole
+}
